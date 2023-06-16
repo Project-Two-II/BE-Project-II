706139be9a655646eb2e4f3be0bf3ae8ca282b97
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-16 18:11:57 +0545

    typo fixed

--- server/subject/apps.py
+++ server/subject/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CourseConfig(AppConfig):
+class SubjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subject'
