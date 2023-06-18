6ffa786fbf3430b6c8760d0e84defb3c1c587703
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-18 08:16:40 +0545

    register bd models in admin site

--- server/subject/admin.py
+++ server/subject/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Subject, Chapter, Question, Test
+
 # Register your models here.
+
+admin.site.register(Subject)
+admin.site.register(Chapter)
+admin.site.register(Question)
+admin.site.register(Test)
