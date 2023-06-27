a404315b904d710e7482bfd09e7e1f5e53e69980
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-27 15:14:35 +0545

    refactor: format changed

--- server/subject/urls.py
+++ server/subject/urls.py
@@ -11,11 +11,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", SubjectListApiView.as_view(), name="subject-list"),
-    path("<int:subject_id>/", SubjectDetailApiView.as_view(), name="subject-details"),
-    path("<int:subject_id>/chapters/", ChapterListApiView.as_view(), name="chapter-list"),
-    path("<int:subject_id>/chapters/<int:chapter_id>/", ChapterDetailApiView.as_view(), name="chapter-details"),
-    path("<int:subject_id>/chapters/<int:chapter_id>/questions/", QuestionListApiView.as_view(), name="question-list"),
+    path("",
+         SubjectListApiView.as_view(), name="subject-list"),
+    path("<int:subject_id>/",
+         SubjectDetailApiView.as_view(), name="subject-details"),
+    path("<int:subject_id>/chapters/",
+         ChapterListApiView.as_view(), name="chapter-list"),
+    path("<int:subject_id>/chapters/<int:chapter_id>/",
+         ChapterDetailApiView.as_view(), name="chapter-details"),
+    path("<int:subject_id>/chapters/<int:chapter_id>/questions/",
+         QuestionListApiView.as_view(), name="question-list"),
     path("<int:subject_id>/chapters/<int:chapter_id>/questions/<int:question_id>/",
          QuestionDetailApiView.as_view(), name="question-detail"),
     path("<int:subject_id>/chapters/<int:chapter_id>/questions/<int:question_id>/test/",
