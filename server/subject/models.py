a3fb49ebcca07a9220e09611512aae619b009cc9
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-27 06:09:33 +0545

    teachers and students fields are added

--- server/subject/models.py
+++ server/subject/models.py
@@ -1,20 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 from userauth.models import User
 
-SUB_THUMB_DIR = "subject_thumb/"
-DEFAULT_SUB_THUMB = "subject_thumb/default.png"
+DEFAULT_SUB_THUMB = "subject_thumbnails/default.png"
+
+
+def upload_to_subject_thumbnails(instance, filename):
+    """
+    Define the upload path for Subject thumbnail image
+    """
+    return f"subject_thumbnails/{instance.code_no}/{filename}"
 
 
 class Subject(models.Model):
     """
     This is database table for Subject.
+    There can be more than one teacher within a subject
+    There is a group associated with the subject which we called students.
+    When we delete Subject related Group of students will be removed.
     """
-    code_no = models.CharField(unique=True, max_length=50)
-    title = models.CharField(unique=True, max_length=255)
-    users = models.ManyToManyField(User)
+    code_no = models.CharField(unique=True, max_length=10)
+    title = models.CharField(unique=True, max_length=150)
+    teachers = models.ManyToManyField(User, related_name="teachers")
+    students = models.OneToOneField(Group, on_delete=models.CASCADE, null=True, blank=True, related_name="students")
     description = models.TextField()
-    thumbnail = models.ImageField(default=DEFAULT_SUB_THUMB, upload_to=SUB_THUMB_DIR, blank=True)
+    thumbnail = models.ImageField(default=DEFAULT_SUB_THUMB, upload_to=upload_to_subject_thumbnails, blank=True)
 
     class Meta:
         db_table = "Subject_elabx"
@@ -27,6 +38,7 @@ class Chapter(models.Model):
     """
     This is for Chapter.
     One subject can have multiple chapters.
+    If the subject is deleted, all the chapters will delete.
     """
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -43,6 +55,7 @@ class Question(models.Model):
     """
     This is table for Question.
     One chapter can have multiple questions.
+    If the chapter is deleted, all the questions will delete.
     """
     title = models.CharField(max_length=225, unique=True)
     description = models.TextField()
@@ -59,6 +72,7 @@ class Test(models.Model):
     """
     This table is to store test code.
     One question can have one test.
+    If the question is deleted, test will delete.
     """
     source_code = models.TextField()
     question = models.OneToOneField(Question, on_delete=models.CASCADE, related_name="test")
