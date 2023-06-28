e0ee806a97dc1e6162284decfc31041179695bb8
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-28 22:29:08 +0545

    docs: message passing from User to Profile

--- server/userauth/signals.py
+++ server/userauth/signals.py
@@ -9,10 +9,16 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    If the user is created, automatically create the profile
+    """
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """
+    Save the profile, after creation of User
+    """
     instance.profile.save()
