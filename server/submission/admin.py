4b4813bda42b7cd73048d6aea4a31e0ced8f2b80
Bishal Poudel <bishalpoudel607@gmail.com>
2023-06-30 17:14:11 +0545

    refactor: comment changes into review

--- server/submission/admin.py
+++ server/submission/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Submission, Result, Comment
+from .models import Submission, Result, Review
 
 # Register your models here.
 
 admin.site.register(Submission)
 admin.site.register(Result)
-admin.site.register(Comment)
+admin.site.register(Review)
