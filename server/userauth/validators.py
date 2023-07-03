bb16f85057415fb37853b4c4cb74224fa970f4cc
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-04 00:18:21 +0545

    password validator

---
+++ server/userauth/validators.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+import re
+
+
+class CustomPasswordValidator:
+    def validate(self, password, user=None):
+        if len(password) < 8:
+            raise ValidationError(_("The password must be at least 8 characters long."))
+        if not re.search(r"[a-z]", password):
+            raise ValidationError(_("The password must contain at least one lowercase letter."))
+        if not re.search(r"[A-Z]", password):
+            raise ValidationError(_("The password must contain at least one uppercase letter."))
+        if not re.search(r"\d", password):
+            raise ValidationError(_("The password must contain at least one digit."))
+        if not re.search(r"[!@#$%^&*()_+|{}:;']", password):
+            raise ValidationError(_("The password must contain at least one special letter."))
+
+    def get_help_text(self):
+        return _("Your password must contain at least 8 characters including one uppercase character, "
+                 "one lowercase character, one digit and one special character.")
