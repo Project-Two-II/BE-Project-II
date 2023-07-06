cff21576ab2059f453098e008d49628518212695
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-06 17:41:07 +0545

    feat: signal for Automatically create group after creation of subject

---
+++ server/subject/signals.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Subject, SubjectGroup
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=Subject)
+def create_group(sender, instance, created, **kwargs):
+    """
+    Automatically create a group for a subject when it is created.
+    """
+    if created:
+        group_name = f"{instance.code_no}_group"
+        SubjectGroup.objects.create(name=group_name, subject=instance)
