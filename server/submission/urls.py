de957d37a9e3e12e102f82c6fda37ee0a08e4133
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-15 20:00:07 +0545

    updated url path for submission app

--- server/submission/urls.py
+++ server/submission/urls.py
@@ -5,8 +5,13 @@ from .views import SubmissionAPIView, ResultAPIView, ReviewListCreateAPIView, Re
 app_name = "submission"
 
 urlpatterns = [
-    path("", SubmissionAPIView.as_view(), name="submission"),
-    path("result/", ResultAPIView.as_view(), name="result"),
-    path("review/", ReviewListCreateAPIView.as_view(), name="review-list"),
-    path("review/<int:review_id>", ReviewRetrieveUpdateDestroyAPIView.as_view(), name="review-detail")
+    path("<int:subject_id>/chapters/<int:chapter_id>/questions/<int:question_id>/submit/",
+         SubmissionAPIView.as_view(), name="submission"),
+    path("<int:subject_id>/chapters/<int:chapter_id>/questions/<int:question_id>/submit/<int:submission_id>/result/",
+         ResultAPIView.as_view(), name="result"),
+    path("<int:subject_id>/chapters/<int:chapter_id>/questions/<int:question_id>/submit/<int:submission_id>/review/",
+         ReviewListCreateAPIView.as_view(), name="review-list"),
+    path("<int:subject_id>/chapters/<int:chapter_id>/questions/"
+         "<int:question_id>/submit/<int:submission_id>/review/<int:review_id>",
+         ReviewRetrieveUpdateDestroyAPIView.as_view(), name="review-detail")
 ]
