ee8a40b46b44af3ad8916d247ba3254bc2cd2bcd
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-15 19:59:50 +0545

    updated submission views

--- server/submission/views.py
+++ server/submission/views.py
@@ -18,20 +18,17 @@ class SubmissionAPIView(APIView):
     """
     API endpoint for Solution to a question related activities
     """
-    permission_classes = (IsAuthenticated, SubjectDetailAccessPermission)
+    # permission_classes = (IsAuthenticated, SubjectDetailAccessPermission)
 
-    def get(self, request, *args, **kwargs):
-        submissions = Submission.objects.all()
+    def get(self, request, subject_id, chapter_id, question_id, *args, **kwargs):
+        chapter = Chapter.objects.get(id=chapter_id, subject=subject_id)
+        question = Question.objects.get(id=question_id, chapter=chapter.id)
+        submissions = Submission.objects.get(question=question.id)
         serializer = SubmissionSerializer(submissions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, subject_id, chapter_id, question_id, *args, **kwargs):
         data = request.data.copy()
-        # If data are send in JSON payload
-        subject_id = data.pop("subject", None)
-        chapter_id = data.pop("chapter", None)
-        question_id = data.pop("question", None)
-
         # Validate and check if the question exists,
         # if yes assign this submission to this question by requested user
         try:
@@ -54,33 +51,61 @@ class SubmissionAPIView(APIView):
 class ResultAPIView(APIView):
     permission_classes = (IsAuthenticated, SubjectDetailAccessPermission)
 
-    def get(self, request, *args, **kwargs):
-        results = Result.objects.all()
-        serializer = ResultSerializer(results, many=True)
+    def get(self, request, subject_id, chapter_id, question_id, submission_id, *args, **kwargs):
+        chapter = Chapter.objects.get(id=chapter_id, subject=subject_id)
+        question = Question.objects.get(id=question_id, chapter=chapter.id)
+        submission = Submission.objects.get(question=question.id, id=submission_id)
+        result = Result.objects.get(submission=submission.id)
+        serializer = ResultSerializer(result)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
-        data = request.data.copy()
-
-        submission_id = data.pop("submission", None)
+    def post(self, request, subject_id, chapter_id, question_id, submission_id, *args, **kwargs):
+        payload = request.data.copy()
         try:
-            submission = Submission.objects.get(id=submission_id)
-        except Result.DoesNotExist:
-            return Response({"error": "Invalid Submission or Result"}, status=status.HTTP_400_BAD_REQUEST)
+            chapter = Chapter.objects.get(id=chapter_id, subject=subject_id)
+            question = Question.objects.get(id=question_id, chapter=chapter.id)
+            submission = Submission.objects.get(question=question.id, id=submission_id)
+        except Submission.DoesNotExist:
+            return Response({"error": "Invalid Submission or Subject"}, status=status.HTTP_400_BAD_REQUEST)
 
-        data["submission"] = submission.id
+        payload["submission"] = submission.id
 
-        serializer = SubmissionSerializer(data=data, partial=True)
+        serializer = ResultSerializer(data=payload, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ReviewListCreateAPIView(ListCreateAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+class ReviewListCreateAPIView(APIView):
     permission_classes = (IsAuthenticated, SubjectDetailAccessPermission)
+
+    def get(self, request, subject_id, chapter_id, question_id, submission_id, *args, **kwargs):
+        chapter = Chapter.objects.get(id=chapter_id, subject=subject_id)
+        question = Question.objects.get(id=question_id, chapter=chapter.id)
+        submission = Submission.objects.get(question=question.id, id=submission_id)
+        review = Review.objects.get(submission=submission.id)
+        serializer = ResultSerializer(review)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, subject_id, chapter_id, question_id, submission_id, *args, **kwargs):
+        payload = request.data.copy()
+        try:
+            chapter = Chapter.objects.get(id=chapter_id, subject=subject_id)
+            question = Question.objects.get(id=question_id, chapter=chapter.id)
+            submission = Submission.objects.get(id=submission_id, question=question.id)
+        except Submission.DoesNotExist or Question.DoesNotExist or Chapter.DoesNotExist:
+            return Response({"error": "Invalid Subject or Chapter or Question or Submission"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        payload["submission"] = submission.id
+        payload["reviewed_by"] = self.request.user.id
+
+        serializer = ReviewSerializer(data=payload)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ReviewRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
