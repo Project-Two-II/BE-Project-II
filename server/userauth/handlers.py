19c4386ffff5e938f6a6d3609c80aabcf1b2498f
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-15 19:04:11 +0545

    refactor: custom exception handler

--- server/userauth/handlers.py
+++ server/userauth/handlers.py
@@ -1,11 +1,15 @@
 from rest_framework.views import exception_handler
-from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.exceptions import MethodNotAllowed, NotAuthenticated
 from rest_framework.response import Response
 
 
 def custom_exception_handler(exc, context):
     if isinstance(exc, MethodNotAllowed) and context["request"].method == "GET":
-        return Response({"detail": "GET method is not allowed."},
+        return Response({"error": "GET method is not allowed."},
+                        status=exc.status_code)
+
+    if isinstance(exc, NotAuthenticated):
+        return Response({"error": "Authentication credentials were not provided."},
                         status=exc.status_code)
 
     # For all other exceptions, fallback to the default exception handler
