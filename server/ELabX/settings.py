254e96a002a23bbf58f97e30ea7abb1a6ab0667d
Bishal Poudel <bishalpoudel607@gmail.com>
2023-07-24 19:21:56 +0545

    feat: Verify account thru email

--- server/ELabX/settings.py
+++ server/ELabX/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@y9++&k#xav=gfjwbfdfs5a!kb&#2ks8ukysc*d#@==!mj$%m#'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'ELabX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elabx_db',
-        'USER': 'elabx',
-        'PASSWORD': 'elabx_password',
-        'HOST': 'elabx_db',
-        'PORT': 3306,
+        'ENGINE': config("DB_ENGINE"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
@@ -188,3 +188,10 @@ SIMPLE_JWT = {
 }
 
 AUTHENTICATION_BACKENDS = ["userauth.backends.EmailBackend"]
+
+# Credentials to send verification email
+EMAIL_USE_SSL = True
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
