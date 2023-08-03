20d7567ebc0699850d5c6e2700d0c2a20e2b2c63
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-08-03 11:29:20 +0545

    feat: run worker on editor load

--- client/elabx/src/Components/Editor.jsx
+++ client/elabx/src/Components/Editor.jsx
@@ -1,4 +1,5 @@
 import {React, useState, useEffect} from 'react'
+import { useSelector } from 'react-redux';
 import EditorWindow from "@monaco-editor/react";
 
 import WorkerAPI from '../shared_web.js'
@@ -9,6 +10,17 @@ const initialHeader = "#include <iostream>\n";
 
 function Editor({defaultLang}) {
 
+  const token = useSelector((state) =>  state.token);
+
+  
+  const fetchOption = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": "Bearer  " +  token
+    },
+  }
+
   const [code, setCode] = useState("")
   const [testCode, setTestCode] = useState("");
 
@@ -17,9 +29,10 @@ function Editor({defaultLang}) {
   }
 
   function getData() {
-    fetch("http://localhost:8000/api/subjects/1/chapters/1/questions/1/test/")
+    fetch("http://localhost:8000/api/subjects/1/chapters/1/questions/2/test/", fetchOption)
         .then(resp => resp.json())
         .then(data => {
+          console.log(data)
           setTestCode(data.source_code)
         })
         .catch(err => console.log(err))
@@ -27,7 +40,7 @@ function Editor({defaultLang}) {
   
   const RunClickHandler = (e) => {
     e.preventDefault();
-    let finalCode = initialHeader + code;
+    let finalCode = initialHeader + code + testCode;
     let API = new WorkerAPI();
     API.compileLinkRun(finalCode);
   }
