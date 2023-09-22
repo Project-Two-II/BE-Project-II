f3d6b9d0a6de43e8de5a8f3bb4345b5f1da10da5
Riya Pant <pantriyapant@gmail.com>
2023-09-22 16:30:25 +0545

    my courses and all courses display success'

--- client/elabx/src/Components/HomePage/homepage.jsx
+++ client/elabx/src/Components/HomePage/homepage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import CourseList from '../CourseList.jsx'
 import { Link } from 'react-router-dom'
+
+import MyCourseList from '../myCourseList.jsx'
+import CourseList from '../CourseList.jsx'
 import './homepage.css'
 import '../../App.css'
 
@@ -37,11 +39,11 @@ function HomePage() {
         <div className="home-container">
           <div className="headingStyle" style={headingStyle}>My Courses</div>
           <div className="courseList">
-            <CourseList token={token} />
+            <MyCourseList token={token} />
           </div>
           <span className="headingStyle" style={headingStyle}>All Courses</span>
           <div className="courseList">
-            <CourseList token={token} />
+            <CourseList token={token}/>
           </div>
         </div>
       ) : (
---
+++ client/elabx/src/Components/myCourseList.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import CourseCard from './CourseCard.jsx';
+
+const course_list_style = {
+  padding: "20px",
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+  gridGap: "40px",
+};
+
+function MyCourseList({ token }) {
+  const [course, setCourse] = useState([]);
+
+  const fetchOption = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": "Bearer " +  token
+    }
+  }
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/subjects/mysubjects/", fetchOption);
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
+        setCourse(data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
+    };
+
+    fetchCourses();
+  }, [token]);
+
+  return (
+    <div className='course-list' style={course_list_style}>
+      {course.map((course) => (
+        <CourseCard key={course.id} course={course} />
+      ))}
+    </div>
+  );
+}
+
+export default MyCourseList;
